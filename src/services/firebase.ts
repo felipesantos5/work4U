ac1f0fd3bd15ec3a2dc20239fe4084424c7ed992
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-09-12 01:43:59 -0300

    feat: firebase config

---
+++ src/services/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLEYE1NJUh4HDCvLoToSCwtBHFqlfKWZ8",
+  authDomain: "work4u-904e1.firebaseapp.com",
+  projectId: "work4u-904e1",
+  storageBucket: "work4u-904e1.appspot.com",
+  messagingSenderId: "236613600438",
+  appId: "1:236613600438:web:a2bbd48e0241d6ec659217",
+  measurementId: "G-V0ER1NC7W9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
