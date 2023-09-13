7a9c622e109611d607cb538813475031bcc99487
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2023-09-13 16:32:35 -0300

    refactor: changing the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,22 +1,27 @@
 import { BiSolidUserCircle } from "react-icons/bi";
-
 import logo from "../../img/w4u-logo.png";
 import { Link } from "react-router-dom";
+
+// const handleLogout = () => {
+//   setUser(null);
+// };
 
 export const Header = () => {
   return (
     <header className="">
       <section className="flex justify-between items-center max-w-7xl m-auto py-4">
-        <img src={logo} alt="logo" className="w-44" />
+        <Link to={"/"}>
+          <img src={logo} alt="logo" className="w-44" />
+        </Link>
         <nav className="flex gap-6 items-center text-lg">
           <Link to={"/"}>home</Link>
           <Link to={"/register"}>registro</Link>
           <Link to={"/login"}>login</Link>
 
-          <div className="flex items-center gap-2">
+          <Link className="flex items-center gap-2" to={"/profile"}>
             <BiSolidUserCircle size={30} />
             <p>name</p>
-          </div>
+          </Link>
         </nav>
       </section>
     </header>
